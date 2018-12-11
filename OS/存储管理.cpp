822dfbab1df8fe12dc472bb76cd8163f79df8bbb
zhangabing <2411035458@qq.com>
2018-12-11 22:16:12 +0800

    起步

---
+++ OS/存储管理.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct jcb {
+	char name[20];
+	int begintime;
+	int endtime
+	int memory;
+};
+
+struct node {
+
+
+};
+
+// 首次适应算法
+void firstFit() {
+
+}
+
+// 最佳适应算法
+void bestFit() {
+
+}
+
+int main() {
+
+	return 0;
+}
